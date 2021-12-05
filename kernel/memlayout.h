6f2dc6e34f99f59d88697a2ff7030dd49422a589
Ole Sivert Aarhaug <ole.sivert@gmail.com>
2021-12-06 00:29:21 +0100

    Changed back to full memory layout

--- kernel/memlayout.h
+++ kernel/memlayout.h
@@ -45,8 +45,8 @@
 // for use by the kernel and user pages
 // from physical address 0x80000000 to PHYSTOP.
 #define KERNBASE 0x80000000L
-//#define PHYSTOP (KERNBASE + 32*1024*1024)
-#define PHYSTOP (KERNBASE + 2*1024*1024)
+#define PHYSTOP (KERNBASE + 32*1024*1024)
+//#define PHYSTOP (KERNBASE + 2*1024*1024)
 
 // map the trampoline page to the highest address,
 // in both user and kernel space.
